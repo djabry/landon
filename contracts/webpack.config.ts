69e4078c9d928dc390d66a6925aad00482e6ae42
Daniel Jabry <daniel.jabry@rezonence.com>
2018-10-27 22:18:53 +0100

    Move contracts files to new top folder and correct build script

--- contracts/webpack.config.ts
+++ contracts/webpack.config.ts
@@ -2,10 +2,11 @@ import {resolve} from 'path';
 import * as CleanWebpackPlugin from 'clean-webpack-plugin';
 import * as nodeExternals from 'webpack-node-externals';
 import * as ZipPlugin from 'zip-webpack-plugin';
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const distFolder = resolve(__dirname, 'dist');
 const config = {
-  entry: resolve(__dirname, 'src', 'contracts', 'index.ts'),
+  entry: resolve(__dirname, 'index.ts'),
   target: 'node',
   output: {
     path: distFolder,
@@ -29,6 +30,7 @@ const config = {
     ]
   },
   plugins: [
+    new CopyWebpackPlugin([ {from: 'package.json'}]),
     new ZipPlugin({filename: 'dist.zip'}),
     new CleanWebpackPlugin([distFolder])
   ]
